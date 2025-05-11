367d2eefa3309eb68608890a15c274a61709e57b
Dulaksha Dulan <dulakshadulan@gmail.com>
2025-05-11 21:14:26 +0530

    soda

--- hungry.py
+++ hungry.py
@@ -6,6 +6,7 @@ else:
     thirsty = input("are you thirsty?").lower()
     if thirsty == 'yes':
         print('drink water')
+        print('drink soda')
     else:
         print("do your homework")
 print('well done')
