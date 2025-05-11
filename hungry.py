dda3e6295c805e568ca12eef18d65df9e4f8c64b
Dulaksha Dulan <dulakshadulan@gmail.com>
2025-05-11 21:12:27 +0530

    thirsty

--- hungry.py
+++ hungry.py
@@ -1,7 +1,11 @@
-hungry = input("are you hungry?")
-if hungry.lower() == "yes":
+hungry = input("are you hungry?").lower()
+if hungry == "yes":
     print("eat samosa")
     print("eat boba")
 else:
-    print("do your homework")
+    thirsty = input("are you thirsty?").lower()
+    if thirsty == 'yes':
+        print('drink water')
+    else:
+        print("do your homework")
 print('well done')
