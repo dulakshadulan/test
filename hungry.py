cb77a8452e22fbdcad4af6511cbe3c6296bed4b8
Dulaksha Dulan <dulakshadulan@gmail.com>
2025-05-11 15:11:13 +0530

    Revert "more changes"
    This reverts commit d2c1f0fe82e886df132e408aca6b1aea861d9cef.

--- hungry.py
+++ hungry.py
@@ -2,6 +2,7 @@ hungry = input("are you hungry?")
 if hungry.lower() == "yes":
     print("eat samosa")
     print("eat pizza")
+    print("eat BBQ")
 else:
     print("do your homework")
 print('well done')
