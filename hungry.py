e3ccbdc64ac6e4c351eff7f1fece723b502fb054
Dulaksha Dulan <dulakshadulan@gmail.com>
2025-05-11 15:16:06 +0530

    hi

--- hungry.py
+++ hungry.py
@@ -1,8 +1,6 @@
 hungry = input("are you hungry?")
 if hungry.lower() == "yes":
     print("eat samosa")
-    print("eat pizza")
-    print("eat BBQ")
 else:
     print("do your homework")
 print('well done')
