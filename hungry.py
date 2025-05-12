052a26f26bcf6f9714cbee2b6fa5bf48b3fd4a40
Dulaksha Dulan <dulakshadulan@gmail.com>
2025-05-12 22:46:45 +0530

    added yo

--- hungry.py
+++ hungry.py
@@ -1,7 +1,8 @@
 hungry = input("are you hungry?").lower()
-if hungry == "yes":
+if hungry == "yes" and hungry == "yo":
     print("eat samosa")
     print("eat pizza")
+    print("over")
 else:
     thirsty = input("are you thirsty?").lower()
     if thirsty == 'yes':
