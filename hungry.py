ea010b671e423d14499a77dc35275ecc03a19f67
Dulaksha Dulan <dulakshadulan@gmail.com>
2025-05-12 21:02:39 +0530

    Revert "boba"
    This reverts commit b5d0650846078e502ebc79f08f8197150725dccc.

--- hungry.py
+++ hungry.py
@@ -2,7 +2,6 @@ hungry = input("are you hungry?").lower()
 if hungry == "yes":
     print("eat samosa")
     print("eat boba")
-    print("eat fries")
 else:
     print("do your homework")
 print('well done')
